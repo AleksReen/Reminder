a3057d9df1647b09ca0924bc4f79ad4def2f2131
AleksReen <aleksandr.reentovich@tut.by>
2017-11-15 13:47:14 +0300

    [rem - 0.2] added project Reminder.Domain

---
+++ Reminder.Domain/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reminder.Domain
+{
+    public class Class1
+    {
+    }
+}
