d7d258bd92c8f7f0a35d8598c0265bf4fa4e3e1e
AleksReen <aleksandr.reentovich@tut.by>
2017-11-17 15:32:05 +0300

    [rem-0.3] added project Reminder.Business

---
+++ Reminder.Business/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reminder.Business
+{
+    public class Class1
+    {
+        public string N = "wwwww";
+    }
+}
--- Reminder.WebUI/Controllers/ReminderController.cs
+++ Reminder.WebUI/Controllers/ReminderController.cs
@@ -1,4 +1,5 @@
-﻿using Reminder.Domain.FakeModel;
+﻿using Reminder.Business;
+using Reminder.Domain.FakeModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
