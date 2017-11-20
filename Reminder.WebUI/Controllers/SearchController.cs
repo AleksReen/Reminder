0bfb2286c4b2cee2e787a115c74dbcee37520d49
AleksReen <aleksandr.reentovich@tut.by>
2017-11-20 14:58:38 +0300

    [rem-0.3] Added SearchController and connected Ajax

---
+++ Reminder.WebUI/Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reminder.WebUI.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult SearchList()
+        {
+            return View();
+        }
+
+        public ActionResult GetSearchResult(string name, string date, string category)
+        {
+            ViewBag.name = name;
+            ViewBag.date = date;
+            ViewBag.category = category;
+            return PartialView("_SearchResult");
+        }
+    }
+}
