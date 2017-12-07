e0153b99edf5275984bfcdb8def3f29f37c61998
AleksReen <aleksandr.reentovich@tut.by>
2017-12-07 14:38:26 +0300

    [rem-1.1] - added UI Registration

---
+++ Reminder.WebUI/Controllers/RegistrationController.cs
@@ -0,0 +1,24 @@
+﻿using Reminder.WebUI.Models.ViewsModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reminder.WebUI.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(ViewRegistration regForm)
+        {
+            return View();
+        }
+    }
+}
